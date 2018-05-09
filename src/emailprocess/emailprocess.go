591fc619b02fb00a0f42bd7874aecaec4c79c389
croacker <croacker@mail.ru>
2018-05-09 14:06:34 +0800

    work with email

--- src/emailprocess/emailprocess.go
+++ src/emailprocess/emailprocess.go
@@ -1,92 +1,80 @@
 package emailprocess
 
 import (
-	"io"
-	"io/ioutil"
+	// "io"
+	// "io/ioutil"
 	"log"
 
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
-	"github.com/emersion/go-message/mail"
 )
 
-func receive() {
-	// Let's assume c is a client
-	var c *client.Client
+func Receive() {
+	log.Println("Connecting to server...")
+
+	// Connect to server
+	c, err := client.DialTLS("imap.mail.ru:993", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected")
+
+	// Don't forget to logout
+	defer c.Logout()
+
+	// Login
+	if err := c.Login("croacker@mail.ru", ""); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Logged in")
+
+	// List mailboxes
+	mailboxes := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+	go func() {
+		done <- c.List("", "*", mailboxes)
+	}()
+
+	log.Println("Mailboxes:")
+	for m := range mailboxes {
+		log.Println("* " + m.Name)
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
 
 	// Select INBOX
 	mbox, err := c.Select("INBOX", false)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Flags for INBOX:", mbox.Flags)
 
-	// Get the last message
-	if mbox.Messages == 0 {
-		log.Fatal("No message in mailbox")
+	// Get the last 4 messages
+	from := uint32(1)
+	to := mbox.Messages
+	if mbox.Messages > 3 {
+		// We're using unsigned integers here, only substract if the result is > 0
+		from = mbox.Messages - 3
 	}
-	seqSet := new(imap.SeqSet)
-	seqSet.AddNum(mbox.Messages)
+	seqset := new(imap.SeqSet)
+	seqset.AddRange(from, to)
 
-	// Get the whole message body
-	section := &imap.BodySectionName{}
-	items := []imap.FetchItem{section.FetchItem()}
-
-	messages := make(chan *imap.Message, 1)
+	messages := make(chan *imap.Message, 10)
+	done = make(chan error, 1)
 	go func() {
-		if err := c.Fetch(seqSet, items, messages); err != nil {
-			log.Fatal(err)
-		}
+		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
 	}()
 
-	msg := <-messages
-	if msg == nil {
-		log.Fatal("Server didn't returned message")
+	log.Println("Last 4 messages:")
+	for msg := range messages {
+		log.Println("* " + msg.Envelope.Subject)
 	}
 
-	r := msg.GetBody(section)
-	if r == nil {
-		log.Fatal("Server didn't returned message body")
-	}
-
-	// Create a new mail reader
-	mr, err := mail.CreateReader(r)
-	if err != nil {
+	if err := <-done; err != nil {
 		log.Fatal(err)
 	}
 
-	// Print some info about the message
-	header := mr.Header
-	if date, err := header.Date(); err == nil {
-		log.Println("Date:", date)
-	}
-	if from, err := header.AddressList("From"); err == nil {
-		log.Println("From:", from)
-	}
-	if to, err := header.AddressList("To"); err == nil {
-		log.Println("To:", to)
-	}
-	if subject, err := header.Subject(); err == nil {
-		log.Println("Subject:", subject)
-	}
-
-	// Process each message's part
-	for {
-		p, err := mr.NextPart()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatal(err)
-		}
-
-		switch h := p.Header.(type) {
-		case mail.TextHeader:
-			// This is the message's text (can be plain-text or HTML)
-			b, _ := ioutil.ReadAll(p.Body)
-			log.Println("Got text: %v", string(b))
-		case mail.AttachmentHeader:
-			// This is an attachment
-			filename, _ := h.Filename()
-			log.Println("Got attachment: %v", filename)
-		}
-	}
+	log.Println("Done!")
 }
